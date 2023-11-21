970a01314db3d4807642458affdfebfa8cdb61fa
Egarcia99 <emilyo.garcia99@gmail.com>
2023-11-21 13:14:39 -0800

    edited sql tables with proper infomation

--- sql/UBDB_data.sql
+++ sql/UBDB_data.sql
@@ -1,48 +1,48 @@
-Prompt ==== Populating Employee with placeholders =====
+Prompt ==== Populating Employee =====
 
-   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_status)
+   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role)
    VALUES ('JG1234', 'Juan', 'Garcia', 'CEO', 'T');
    
-   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_status)
+   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role)
    VALUES ('CG5678', 'Claudia', 'Garcia', 'CFO', 'T');
    
-   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_status)
-   VALUES ('RA9876', 'Rebeca', 'Alvarado', 'Office', 'T');
+   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_hourly_pay_rate)
+   VALUES ('RA9876', 'Rebeca', 'Alvarado', 'Office', 'T', 22.00);
    
-   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_status)
-   VALUES ('SH5432', 'Susana', 'Hernandez', 'Office', 'T');
+   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_hourly_pay_rate)
+   VALUES ('SH5432', 'Susana', 'Hernandez', 'Office', 'T', 22.00);
    
-   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_status)
-   VALUES ('EM2345', 'Elena', 'Marquez', 'Office', 'T');
+   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_hourly_pay_rate)
+   VALUES ('EM2345', 'Elena', 'Marquez', 'Office', 'T', 19.00);
    
-   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_status)
-   VALUES ('JG7890', 'Jaime', 'Garcia', 'Field', 'T');
+   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_hourly_pay_rate)
+   VALUES ('JG7890', 'Jaime', 'Garcia', 'Field', 'T', 17.00);
    
-   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_status)
-   VALUES ('HG6789', 'Horacio', 'Guerrero', 'Field', 'T');
+   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_hourly_pay_rate)
+   VALUES ('HG6789', 'Horacio', 'Guerrero', 'Field', 'T', 22.50);
    
-   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_status)
-   VALUES ('MR4321', 'Maria', 'Resendez', 'Field', 'T');
+   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_hourly_pay_rate)
+   VALUES ('MR4321', 'Maria', 'Resendez', 'Field', 'T', 16.00);
    
-   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_status)
-   VALUES ('CP8765', 'Cristobal', 'Pascual', 'Field', 'T');
+   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_hourly_pay_rate)
+   VALUES ('CP8765', 'Cristobal', 'Pascual', 'Field', 'T', 50.00);
    
-   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_status)
-   VALUES ('OC2341', 'Oscar', 'Carrillo', 'Field', 'T');
+   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_hourly_pay_rate)
+   VALUES ('OC2341', 'Oscar', 'Carrillo', 'Field', 'T', 32.50);
    
-   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_status)
-   VALUES ('GS7890', 'Gregorio', 'Salazar', 'Field', 'T');
+   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_hourly_pay_rate)
+   VALUES ('GS7890', 'Gregorio', 'Salazar', 'Field', 'T', 32.50);
    
-   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_status)
-   VALUES ('RF5432', 'Raul', 'Ferreira', 'Field', 'T');
+   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_hourly_pay_rate)
+   VALUES ('RF5432', 'Raul', 'Ferreira', 'Field', 'T', 25.00);
    
-   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_status)
-   VALUES ('OG9876', 'Orlando', 'Guzman', 'Field', 'T');
+   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_hourly_pay_rate)
+   VALUES ('OG9876', 'Orlando', 'Guzman', 'Field', 'T', 20.00);
    
-   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_status)
-   VALUES ('GC2345', 'Gabriel', 'Carrillo', 'Field', 'T');
+   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_hourly_pay_rate)
+   VALUES ('GC2345', 'Gabriel', 'Carrillo', 'Field', 'T', 22.50);
    
-   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_status)
-   VALUES ('ER6789', 'Erick', 'Rodriguez', 'Field', 'T');
+   INSERT INTO Employee (EMPL_ID, empl_first_name, empl_last_name, empl_role, empl_hourly_pay_rate)
+   VALUES ('ER6789', 'Erick', 'Rodriguez', 'Field', 'T', 27.50);
 
 
