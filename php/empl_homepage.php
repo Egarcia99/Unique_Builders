7db659476a4aaae01fc971dc0d36ab4e1c1f2a06
Boyd2324 <89481694+Boyd2324@users.noreply.github.com>
2023-11-28 20:56:20 -0800

    Added the emplhome code

--- php/empl_homepage.php
+++ php/empl_homepage.php
@@ -10,36 +10,50 @@
     Fall 2023
     Employeee Homepage (as a function to be called in other pages)
 */
-    session_start();
 
-    // get username from session variable
-    $username = $_SESSION["username"];
 
-    function emplHomepage()
+    function emplHomepage($username)
     {
         ?>
-        <!-- Personalized header because they entered their username -->
-        <h1 id="welcomeheader">Welcome <?= $username ?></h1>
-        <!-- Nav bar adapted from homepage -->
-        <nav>
-            <ul class="nav">
-                <li><a href="../html/homepage.html">Home</a></li>
-                <li><a href="../php/login_start.php">Employee Login</a></li>
-                <li><a href="../php/cust_contact.php">Contact Us</a></li>
-            </ul>
-        </nav>
+        <!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 
-        <table>
-            <tr>
-                <td><a href="../php/work_orders.php">Work Orders</a></td>
-                <td><a href="../php/payroll.php">Payroll</a></td>
-            </tr>
-            <tr>
-                <td><a href="../php/empl_info.php">Employee Information</a></td>
-                <td><a href="../php/logout.php">Log Out</a></td>
-            </tr>
-        </table>
+<!-- cs328 class HTML template: last modified 2023-02-22 -->
 
+<head>
+    <title>Employee Homepage</title>
+    <meta charset="utf-8" />
+
+    <link href="https://nrs-projects.humboldt.edu/~st10/styles/normalize.css"
+    type="text/css" rel="stylesheet" />
+    
+    <link href="../css/login.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+    <!-- Personalized header because they entered their username -->
+    <h1 id="welcomeheader">Welcome [username]</h1>
+    <!-- Nav bar adapted from homepage -->
+    <nav>
+        <ul class="nav">
+            <li><a href="../html/homepage.html">Home</a></li>
+            <li><a href="../php/login_start.php">Employee Login</a></li>
+            <li><a href="../php/cust_contact.php">Contact Us</a></li>
+        </ul>
+    </nav>
+
+    <table class="homepage">
+        <tr class="homepage">
+            <td class="homepage"><a href="../php/work_orders.php">Work Orders</a></td>
+            <td class="homepage"><a href="../php/payroll.php">Payroll</a></td>
+        </tr>
+        <tr class="homepage">
+            <td class="homepage"><a href="../php/empl_info.php">Employee Information</a></td>
+            <td class="homepage"><a href="../php/logout.php">Log Out</a></td>
+        </tr>
+    </table>
+
+</body>
+</html>
         <?php
     }
 ?>
