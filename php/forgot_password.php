cea3d60789d82abb4558bb9cf0807140bc56f176
Boyd2324 <89481694+Boyd2324@users.noreply.github.com>
2023-12-07 23:31:50 -0800

    functioning forgot password

--- php/forgot_password.php
+++ php/forgot_password.php
@@ -11,8 +11,7 @@
     Employeee Login: Forgot Password
     Requirements: 2.1 & 2.2
 */
-
-    session_start();
+    require_once("redirect_user.php");
 ?>
 
 <!DOCTYPE html>
@@ -35,14 +34,10 @@
         <!-- css file adapted from from cs328 homework 4, problem 9 -->
         <link href="../css/login.css" type="text/css" rel="stylesheet" />
 
-
         <!-- Variables for the following JS script -->
-        <script type="text/javascript">
-        var email = "<?php echo htmlspecialchars($_SESSION["email"]); ?>";
-        var phoneNum = "<?php echo htmlspecialchars($_SESSION["phoneNum"]); ?>";
-        </script>
         <!-- js for checking if the form info is correct -->
         <script defer src="../js/validate_contact_info.js" type="text/javascript"></script>
+        
     </head>
     <?php
      
@@ -51,14 +46,23 @@
      {
         ?>
         <h1 id="welcomeheader">Welcome</h1>
+        <nav>
+            <ul class="nav">
+                <li><a href="../html/homepage.html">Home</a></li>
+                <li><a href="../php/login_start.php">Employee Login</a></li>
+                <li><a href="../php/cust_contact.php">Contact Us</a></li>
+            </ul>
+        </nav>
+
         <form method="post" action="">
-            <h2 id="instructionheader">Please Enter Infomation</h2>
+            <h2 id="instructionheader">Please Enter Username</h2>
 
             <label for="username">Username:</label>
             <input type="text" id="username" name="username" class="roundedinput" required="required" />
             <input type="submit" value="Submit" />
         </form>
         <?php
+        
      }
     else
     {
@@ -66,7 +70,7 @@
         // first, get contact info so the js can check if the user enters the correct contact info
         // get username from session variable
         $username = strip_tags($_POST["username"]);
-        $_SESSION["username"] = $username;
+        
         // first, connect to database to get their contact info 
         // use account that can only see usernames & contact info (not an actual account on nrs-projects)
         // set up db connection 
@@ -79,7 +83,7 @@
 
         // check if the entered username exists in the database already or no.
         // set up query string & statement
-        $contactInfoQueryString = "SELECT empl_id, email, phone_number
+        $contactInfoQueryString = "SELECT empl_id , email, phone_number, first_login, is_temporary
                                     FROM Employee
                                     where empl_id = :username";
         $contactInfoStmt = oci_parse($connObj, $contactInfoQueryString);
@@ -89,57 +93,97 @@
         oci_execute($contactInfoStmt, OCI_DEFAULT);
         $usr = NULL;
         // loop through usernames until username is found or all usernames have been checked
-        while(oci_fetch($contactInfoStmt)){
+        while(oci_fetch($contactInfoStmt))
+        {
             $usr = oci_result($contactInfoStmt, 1);  // get next username from database
-            if($usr == $username){
                 // get user's contact info
-                $_SESSION["email"] = oci_result($contactInfoStmt, 2);
-                $_SESSION["phoneNum"] = oci_result($contactInfoStmt, 3);
-                $userFound = false;
-                break;
-            }
+            $email = oci_result($contactInfoStmt, 2);
+            $phoneNum = oci_result($contactInfoStmt, 3);
+            //if a new user not allowed to reset password.
+            $newUser = oci_result($contactInfoStmt,4);
+            //already requested a new password.
+            $isTemporary= oci_result($contactInfoStmt,5);
+        
         }
-
         // free the statement & close the connection
         oci_free_statement($contactInfoStmt);
         oci_close($connObj);
-    ?>
+        if($usr == $username)
+        {
+            if($newUser == "Y")
+            {
+                
+                ?>  
+                    <h1 id="invalidEntryheader">Invalid Entry</h1>
+                    <p id="invalidEntrymessage">First time user not allowed. If you need assistance, please contact the IT admin.</p>
+                <?php
+                    redirect("../php/forgot_password.php");
+            }
+            elseif($isTemporary == "Y")
+            {
+                ?>  
+                    <<form method="post" action="../php/forgot_password.php" id="goToForgotPassword">
+                <table class="infoTable">
+                    <tr> <td style="color:white;" >Hello <?= $username ?></td> </tr>
+                    <tr> <td style="color:white;">We could not send the admin an email 
+                        to help you get a temporary password at this time.</td> </tr>
+                </table>
+                </form>
+                <?php
+                    redirect("../php/forgot_password.php");
+            }
+            else
+            {
+                ?>
+                <body>
+                        <?php
+                            echo $phoneNum
+                        ?>
 
-    <body>
-            <?php
-            // forgot password: enter contact info to receive temp password later
-            // initialize this to prepare for next page
-            $_SESSION["infoSent"] = "False";
+                        <!-- Personalized header because they entered their username -->
+                        <h1 id="welcomeheader">Welcome <?= $username ?></h1>
+                        <!-- Nav bar adapted from homepage -->
+                        <nav>
+                            <ul class="nav">
+                                <li><a href="../html/homepage.html">Home</a></li>
+                                <li><a href="../php/login_start.php">Employee Login</a></li>
+                                <li><a href="../php/cust_contact.php">Contact Us</a></li>
+                            </ul>
+                        </nav>
+
+                        <form method="post" id="contactInfoForm" action="../php/inform_user.php" onsubmit="return correctInfo('<?=$email?>', '<?=$phoneNum?>')">
+                            <h2 id="instructionheader">Please Provide Needed Information Below</h2>
+                            <input type="hidden" name="username" value="<?= $username ?>">
+
+                            <label for="enteredEmail">Email:</label>
+                            <input type="email" id="enteredEmail" name="emailForgotPassword" class="rectangleinput" placeholder="Confirm Email Address" />
+
+                            <p id="or">Or</p>
+
+                            <label for="enteredPhoneNum">Phone Number:</label>
+                            <input type="text" id="enteredPhoneNum" name="phoneNumForgotPassword" class="rectangleinput" placeholder="Confirm Phone Number" />
+                            
+                            <div id="error-message" style="color: red; padding-bottom: 1em;"></div>
+
+                            <input type="submit" id="submit" value="Submit" />
+                        </form> 
+
+                </body>
+                <?php
+            }
+        }
+        else
+        {
             ?>
-
-            <!-- Personalized header because they entered their username -->
-            <h1 id="welcomeheader">Welcome <?= $username ?></h1>
-            <!-- Nav bar adapted from homepage -->
-            <nav>
-                <ul class="nav">
-                    <li><a href="../html/homepage.html">Home</a></li>
-                    <li><a href="../php/login_start.php">Employee Login</a></li>
-                    <li><a href="../php/cust_contact.php">Contact Us</a></li>
-                </ul>
-            </nav>
-
-
-            <form method="post" id="contactInfoForm" action="../php/inform_user.php">
-                <h2 id="instructionheader">Please Provide Needed Information Below</h2>
-
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="emailForgotPassword" class="rectangleinput" placeholder="Confirm Email Address" />
-
-                <p id="or">Or</p>
-
-                <label for="phoneNum">Phone Number:</label>
-                <input type="test" id="phoneNum" name="phoneNumForgotPassword" class="rectangleinput" placeholder="Confirm Phone Number" />
-
-                <input type="submit" id="submit" value="Submit" />
-            </form>     
-
-    </body>
+            <h1 id="notfoundheader">Incorrect information</h1>
+            <p id="notfoundmessage">If you need assistance, please contact the IT admin.</p>
+                
+            
+        <?php
+            redirect("../php/forgot_password.php");
+        }
+        ?>
     </html>
-
     <?php
     }   // end of else for when the form was submitted
+    ?>
--- php/inform_user.php
+++ php/inform_user.php
@@ -1,8 +1,8 @@
 <?php
 /*
     adapted from: CS 328 hw7 problem2
-    by: Gracie Ceja & Emilyo Garcia
-    last modified: November 24, 2023
+    by: Gracie Ceja & Emilyo Garcian & Colton Boyd
+    last modified: December 7, 2023
 
     you can run this using the URL: https://nrs-projects.humboldt.edu/~glc47/public_html/Unique_Builders-main/php/inform_user.php
     CS 458 Software Engineering
@@ -12,12 +12,13 @@
     Employeee Login: Inform User that info was sent to Admin so they can reset their password
     Requirements: 2.1 & 2.2
 */
-    session_start();
-         // time to contact admin for help, but first:
-        // get info from previous page:
-        // get username & infoSent from session variables
-        $username = $_SESSION["username"];
-        $infoSent = $_SESSION["infoSent"]; 
+    
+        
+    // email to admin to ask them to help user to reset password
+    // this should send to admin's email, but I'll use mine for now
+    if($_SERVER['REQUEST_METHOD'] === 'POST') 
+    {   
+        $username = $_POST["username"];
         // get contact info from form, but first determine which was entered
         if(null != trim($_POST["emailForgotPassword"]))
         {
@@ -29,112 +30,61 @@
             $contactInfo = $_POST["phoneNumForgotPassword"];
             $contactType = "phone number";
         }
-    // email to admin to ask them to help user to reset password
-    // this should send to admin's email, but I'll use mine for now
-    if($_SERVER['REQUEST_METHOD'] === 'POST' && $infoSent == "False") 
-    {
-    
         require_once("../../../private/assign_temp_pass.php");
         generateTempPassword($username, $contactType, $contactInfo);
-        $infoSent = "True";
-        $_SESSION["infoSent"] = "True";
-    }
-    // don't send the email again
-    elseif($infoSent == "True")
-    {
-        $infoSent = "True";
-        $_SESSION["infoSent"] = "True";
-    }
-    else
-    {
-        $infoSent = "False";
-        $_SESSION["infoSent"] = "False";
-    }
+        
+   ?>
+        <!DOCTYPE html>
+        <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 
+        <!-- cs328 class HTML template: last modified 2023-02-22 -->
+
+        <head>
+            <title>Employee Login | Unique Builders</title>
+            <meta charset="utf-8" />
+
+            <!-- favicon -->
+            <link rel="icon" type="image/x-icon" href="../images/UB_logo.jpg" />
+
+            <!-- default css to make the webpage look nearly the same on all browsers -->
+            <link href="https://nrs-projects.humboldt.edu/~st10/styles/normalize.css"
+                type="text/css" rel="stylesheet" />
+                
+
+            <!-- css file adapted from from cs328 homework 4, problem 9 -->
+            <link href="../css/login.css" type="text/css" rel="stylesheet" />
+
+        </head>
+        <body>
+            <?php
+                // inform user that info was sent to admin to make new account
+                ?>
+                    <!-- Personalized header because they entered their username -->
+                    <h1 id="welcomeheader">Thank You <?= $username ?></h1>
+                    <!-- Nav bar adapted from homepage -->
+                    <nav>
+                        <ul class="nav">
+                            <li><a href="../html/homepage.html">Home</a></li>
+                            <li><a href="../php/login_start.php">Employee Login</a></li>
+                            <li><a href="../php/cust_contact.php">Contact Us</a></li>
+                        </ul>
+                    </nav>
+
+                    <form method="post" action="../php/login_start.php" id="goToLogin">
+                        <table class="infoTable">
+                            <tr> <td>Hello <?= $username ?></td> </tr>
+                            <tr> <td>Please be patient as we send you an email or text message 
+                                with a temporary password for the next time you want to login.</td> </tr>
+                            <tr> <td>Information will be sent to:
+                                <?= $contactInfo ?></td> </tr>
+                        </table>
+                        
+                        <!-- button to go back to login -->
+                        <input type="submit" name="submit" value="Go back to Login" />
+                    </form>
+
+        </body>
+        </html>
+<?php
+    }
 ?>
-
-<!DOCTYPE html>
-<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
-
-<!-- cs328 class HTML template: last modified 2023-02-22 -->
-
-<head>
-    <title>Employee Login | Unique Builders</title>
-    <meta charset="utf-8" />
-
-    <!-- favicon -->
-    <link rel="icon" type="image/x-icon" href="../images/UB_logo.jpg" />
-
-	<!-- default css to make the webpage look nearly the same on all browsers -->
-    <link href="https://nrs-projects.humboldt.edu/~st10/styles/normalize.css"
-          type="text/css" rel="stylesheet" />
-          
-
-	<!-- css file adapted from from cs328 homework 4, problem 9 -->
-    <link href="../css/login.css" type="text/css" rel="stylesheet" />
-
-</head>
-<body>
-    <?php
-        // inform user that info was sent to admin to make new account
-        if($infoSent === "True")
-        {
-            $_SESSION["infoSent"] = "True";
-        ?>
-            <!-- Personalized header because they entered their username -->
-            <h1 id="welcomeheader">Thank You <?= $username ?></h1>
-            <!-- Nav bar adapted from homepage -->
-            <nav>
-                <ul class="nav">
-                    <li><a href="../html/homepage.html">Home</a></li>
-                    <li><a href="../php/login_start.php">Employee Login</a></li>
-                    <li><a href="../php/cust_contact.php">Contact Us</a></li>
-                </ul>
-            </nav>
-
-            <form method="post" action="../php/login_start.php" id="goToLogin">
-                <table class="infoTable">
-                    <tr> <td>Hello <?= $username ?></td> </tr>
-                    <tr> <td>Please be patient as we send you an email or text message 
-                        with a temporary password for the next time you want to login.</td> </tr>
-                    <tr> <td>Information will be sent to:
-                        <?= $contactInfo ?></td> </tr>
-                </table>
-                
-                <!-- button to go back to login -->
-                <input type="submit" name="submit" value="Go back to Login" />
-            </form>
-        <?php
-        }   // end of if for when info was sent
-        else
-        {
-            ?>
-            <!-- Personalized header because they entered their username -->
-            <h1 id="welcomeheader">Thank You <?= $username ?></h1>
-            <!-- Nav bar adapted from homepage -->
-            <nav>
-                <ul class="nav">
-                    <li><a href="../html/homepage.html">Home</a></li>
-                    <li><a href="../php/login_start.php">Employee Login</a></li>
-                    <li><a href="../php/cust_contact.php">Contact Us</a></li>
-                </ul>
-            </nav>
-
-            <form method="post" action="../php/forgot_password.php" id="goToForgotPassword">
-                <table class="infoTable">
-                    <tr> <td style="color:white;" >Hello <?= $username ?></td> </tr>
-                    <tr> <td style="color:white;">Please be patient as we could not send the admin an email 
-                        to help you get a temporary password at this time.</td> </tr>
-                    <tr> <td style="color:white;">Please go back and try again.</td> </tr>
-                </table>
-                
-                <!-- button to go back to login -->
-                <input type="submit" name="submit" value="Go back to Forgot Password form" />
-            </form>
-        <?php     
-        }
-    ?>
-
-
-</body>
-</html>
